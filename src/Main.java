3a215339ec24caddf50f0c1d1524eac472647528
Giat00 <adigiat00@gmail.com>
2022-06-05 15:59:27 +0800

    membuat helper connection dan model pengguna

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import frame.PenggunaCreateForm;
+import frame.PenggunaCreateFrame;
+import frame.PenggunaViewForm;
+
+import javax.swing.*;
+
 
 /**
  *
  * @author GIAT
  */
 public class Main {
-    
+    public static void main(String[] args) {
+//        PenggunaCreateFrame frame = new PenggunaCreateFrame();
+//        frame.meletakkanKomponen();
+//        frame.menambahkanListener();
+//        frame.setVisible(true);
+
+
+
+        PenggunaViewForm vf = new PenggunaViewForm();
+        vf.setContentPane(vf.panelMain);
+        vf.pack();
+        vf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        vf.setLocationRelativeTo(null);
+        vf.setVisible(true);
+    }
 }
